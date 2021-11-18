efe932a34875b1cf964b5415882061036d7bb8da
Jordan Leppert <jordanleppert@gmail.com>
2021-11-18 18:31:16 +0000

    Dummy method in Backup

--- btrfssnapshotmanager/backups.py
+++ btrfssnapshotmanager/backups.py
@@ -44,6 +44,9 @@ class Backup():
         # Declare successful sync
         cmd("sudo touch {0}/.lastsync".format(self.subvol.path))
 
+    def location(self):
+        raise Exception("Method must be overridden")
+
     def ensure_target_exists(self):
         raise Exception("Method must be overridden")
 
