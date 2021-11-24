03597b9416b1d41499e85994c48471eb22ac7e25
Jordan Leppert <jordanleppert@gmail.com>
2021-11-24 18:37:02 +0000

    Rsync backups - Uploads are to .tmp dir, then moved after successful upload

--- btrfssnapshotmanager/backups.py
+++ btrfssnapshotmanager/backups.py
@@ -158,7 +158,7 @@ class LocalBtrfsBackup(LocalBackup):
         cmd("btrfs send -p {0} {1} | btrfs receive {2}".format(previous_source.path, source.path, self.path))
 
     def delete_target(self, target_name):
-        info("Deleting snapshot {0} on target {1}".format(target_name, self.location()))
+        info("Deleting via btrfs snapshot {0} on target {1}".format(target_name, self.location()))
         cmd("btrfs subvolume delete --commit-each {0}".format(PosixPath(self.path, target_name)))
 
 
@@ -184,40 +184,84 @@ class RemoteBtrfsBackup(RemoteBackup):
 
 # Rsync
 
-class LocalRsyncBackup(LocalBackup):
+class RsyncBackup():
+
+    def temp_path(self):
+        return PosixPath(self.path, '.tmp')
+
+    def temp_location(self):
+        raise Exception("Method must be overridden")
+
+
+class LocalRsyncBackup(LocalBackup, RsyncBackup):
 
     mechanism = 'rsync'
 
+    def temp_location(self):
+        return self.temp_path()
+
+    def ensure_target_exists(self):
+        super().ensure_target_exists()
+        if not self.temp_path().is_dir():
+            info("Target temp location doesn't exist, creating {0}".format(self.temp_path()))
+            self.temp_path().mkdir(mode=0o700, parents=True)
+
     def transfer_source(self, source):
-        info("Transferring via rsync snapshot {0} to target {1}".format(source.path, self.location()))
-        cmd("rsync -a --delete {0} {1}/".format(source.path, self.path))
+        info("Transferring via rsync snapshot {0} to target temp {1}".format(source.path, self.temp_location()))
+        cmd("rsync -a --delete {0} {1}/".format(source.path, self.temp_path()))
+        self.move_target_snapshot_from_temp(source)
 
     def transfer_source_delta(self, previous_source, source):
-        info("Transferring via rsync snapshot {0} (as delta from {1}) to target {2}".format(source.path, previous_source.path, self.location()))
-        cmd("rsync -a --delete --link-dest={1}/{2}/ {0}/ {1}/{3}".format(source.path, self.path, previous_source.name, source.name))
+        info("Transferring via rsync snapshot {0} (as delta from {1}) to target temp {2}".format(source.path, previous_source.path, self.temp_location()))
+        cmd("rsync -a --delete --link-dest={4}/{2}/ {0}/ {1}/{3}".format(source.path, self.temp_path(), previous_source.name, source.name, self.path))
+        self.move_target_snapshot_from_temp(source)
 
     def delete_target(self, target_name):
-        info("Deleting snapshot {0} on target {1}".format(target_name, self.location()))
+        info("Deleting via rsync snapshot {0} on target {1}".format(target_name, self.location()))
         cmd("rm -rf {0}".format(PosixPath(self.path, target_name)))
 
+    def move_target_snapshot_from_temp(self, source):
+        move_from = PosixPath(self.temp_path(), source.name)
+        move_to = PosixPath(self.path, source.name)
+        info("Moving via rsync target snapshot {0} to {1}".format(move_from, move_to))
+        cmd("mv {0} {1}".format(move_from, move_to))
 
-class RemoteRsyncBackup(RemoteBackup):
+
+class RemoteRsyncBackup(RemoteBackup, RsyncBackup):
 
     mechanism = 'rsync'
 
+    def temp_location(self):
+        return "{0}:{1}".format(self.host, self.temp_path())
+
+    def ensure_target_exists(self):
+        super().ensure_target_exists()
+        exists = cmd("{0} \"if [[ -d '{1}' ]] ; then echo 'yes' ; fi\"".format(self._ssh_command(), self.temp_path()), attempts=self.cmd_attempts, fail_delay=self.cmd_fail_delay)
+        if exists != 'yes':
+            info("Target location doesn't exist, creating {0}".format(self.location()))
+            cmd("{0} \"sudo mkdir -p {1}\"".format(self._ssh_command(), self.temp_path()), attempts=self.cmd_attempts, fail_delay=self.cmd_fail_delay)
+
     def transfer_source(self, source):
-        info("Transferring via rsync snapshot {0} to target {1}".format(source.path, self.location()))
+        info("Transferring via rsync snapshot {0} to target temp {1}".format(source.path, self.temp_location()))
         cmd("rsync -a --delete --rsync-path=\"sudo rsync\" -e \"ssh {0} -l {1}\" {3} {2}:{4}/".format(
-                self.ssh_options, self.user, self.host, source.path, self.path),
+                self.ssh_options, self.user, self.host, source.path, self.temp_path()),
             attempts=self.cmd_attempts, fail_delay=self.cmd_fail_delay)
+        self.move_target_snapshot_from_temp(source)
 
     def transfer_source_delta(self, previous_source, source):
-        info("Transferring via rsync snapshot {0} (as delta from {1}) to target {2}".format(source.path, previous_source.path, self.location()))
-        cmd("rsync -a --delete --link-dest={5}/{6}/ --rsync-path=\"sudo rsync\" -e \"ssh {0} -l {1}\" {3}/ {2}:{5}/{4}/".format(
-                self.ssh_options, self.user, self.host, source.path, source.name, self.path, previous_source.name),
+        info("Transferring via rsync snapshot {0} (as delta from {1}) to target temp {2}".format(source.path, previous_source.path, self.temp_location()))
+        cmd("rsync -a --delete --link-dest={7}/{6}/ --rsync-path=\"sudo rsync\" -e \"ssh {0} -l {1}\" {3}/ {2}:{5}/{4}/".format(
+                self.ssh_options, self.user, self.host, source.path, source.name, self.temp_path(), previous_source.name, self.path),
             attempts=self.cmd_attempts, fail_delay=self.cmd_fail_delay)
+        self.move_target_snapshot_from_temp(source)
 
     def delete_target(self, target_name):
         info("Deleting via rsync snapshot {0} on target {1}".format(target_name, self.location()))
         cmd("{0} \"sudo rm -rf {1}/{2}\"".format(self._ssh_command(), self.path, target_name),
             attempts=self.cmd_attempts, fail_delay=self.cmd_fail_delay)
+
+    def move_target_snapshot_from_temp(self, source):
+        move_from = PosixPath(self.temp_path(), source.name)
+        move_to = PosixPath(self.path, source.name)
+        info("Moving via rsync target snapshot {0} to {1}".format(move_from, move_to))
+        cmd("{0} \"sudo mv {1} {2}\"".format(self._ssh_command(), move_from, move_to))
